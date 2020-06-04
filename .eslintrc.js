5c654343c8e6f92d33e6e68f339d90a94b4dfc18
Evgenii Ray <evgenii.titkov@jetbrains.com>
2020-06-04 18:00:59 +0300

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/display-name': 0,
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'no-console': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
